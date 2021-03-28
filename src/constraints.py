794c179797ed8c652d480c3f4e9fc74c17ad7ad7
filiperecharte <filipeteixeira00@hotmail.com>
2021-03-28 17:11:36 +0100

    update constraints

---
+++ src/constraints.py
@@ -0,0 +1,35 @@
+
+def check_turns(drone_path, problem):
+    final_turn = max(gene.turn for gene in drone_path.genes)
+    if final_turn > problem.turns:
+        return False
+    return True
+
+
+def check_payload(drone_path, problem):
+    payload = 0
+    for gene in drone_path.genes:
+        if gene.demand > 0:
+            payload += gene.demand*problem.products[gene.productID].weight
+        else:
+            if payload > problem.payload:
+                return False
+            payload = 0
+    return True
+
+
+def check_delivery(drone_path):
+    for gene in drone_path.genes:
+        product_qt = 0
+        if gene.demand < 0:
+            for gene1 in drone_path.genes:  # checks previous genes
+                if gene == gene1:
+                    break
+                elif gene1.productID == gene.productID:
+                    product_qt += gene1.demand
+            if product_qt + gene.demand < 0:
+                return False
+    return True
+
+
+
